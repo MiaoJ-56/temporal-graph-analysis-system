24ffbf56ff3ebfa20e07d1273a27567fe76b5ccb
MiaoJ-56 <68734773+MiaoJ-56@users.noreply.github.com>
2024-03-19 09:24:18 +0000

    初步学习

--- Hello.py
+++ Hello.py
@@ -20,26 +20,26 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="首页",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# 欢迎来的时序图分析系统! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    # st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
+        数据、抽象、分析、建模、预测、下一步？
+
+        **👈 快来试一试吧** 
+        
+        ### Who am I?
         - Check out [streamlit.io](https://streamlit.io)
         - Jump into our [documentation](https://docs.streamlit.io)
         - Ask a question in our [community
           forums](https://discuss.streamlit.io)
-        ### See more complex demos
+        ### See more interesting things
         - Use a neural net to [analyze the Udacity Self-driving Car Image
           Dataset](https://github.com/streamlit/demo-self-driving)
         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
